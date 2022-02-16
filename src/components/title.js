6279f0ae1bb243490b8cec37b13f183d2b35aab0
lbaker15 <laelbaker@hotmail.co.uk>
2022-02-16 00:40:06 +0000

    f

--- src/components/title.js
+++ src/components/title.js
@@ -5,18 +5,8 @@ class Title extends React.Component {
     render() {
         return (
             <React.Fragment>
-<<<<<<< HEAD
-<<<<<<< HEAD
-                <div className="title text-center sm:text-left w-full">
-                    The Fiery Bird
-=======
                 <div className="title text-center w-full">
                     The Music Venue
->>>>>>> 23acbb2603d04cc22702b4aea718202bae42ae43
-=======
-                <div className="title text-center w-full">
-                    The Music Venue
->>>>>>> 23acbb2603d04cc22702b4aea718202bae42ae43
                 </div>
             </React.Fragment>
         )
